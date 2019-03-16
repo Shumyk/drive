f33bdcaf1bcdf95c9e1a41ef9995c78e754991d5
Dima Shumyk <dima.shumyk@gmail.com>
2019-03-16 18:25:51 +0200

    Displaying files working

--- drive.js
+++ drive.js
@@ -1,4 +1,7 @@
 let db = {
+    getData: function() {
+        return this.data;
+    },
     addFolder: function(sName) {
         this.data[sName] = {};
     },
@@ -11,13 +14,37 @@ let $ = function (element) {
 
 
 console.log('Hey');
+updateFilesList();
 db.data.some = 'works';
 
 function createItem(sModalId) {
     let inputValue = $('itemName').value;
     $('itemName').value = '';
     db.addFolder(inputValue);
+    updateFilesList();
     closeModal(sModalId);
+}
+
+function updateFilesList() {
+    let filesList = $('files-list');
+    while (filesList.firstChild) {
+        filesList.removeChild(filesList.firstChild);
+    }
+
+    let data = db.getData();
+    for (const key in data) {
+        if (data.hasOwnProperty(key)) {
+            const element = data[key];
+            
+            let div = document.createElement('div');
+            let text = document.createTextNode(key);
+            div.classList.add('data-folder');
+            div.appendChild(text);
+            filesList.appendChild(div); 
+        }
+    }
+
+
 }
 
 
